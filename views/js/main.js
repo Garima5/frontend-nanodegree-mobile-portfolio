b8f178466818ba671ba813e96759c569eca310e9
Garima5 <Garima5@users.noreply.github.com>
2016-10-21 01:21:33 +0530

    Updating updatePosition()

--- views/js/main.js
+++ views/js/main.js
@@ -499,13 +499,18 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
   frame++;
+  //requestAnimationFrame(updatePositions());
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+ // var items = document.querySelectorAll('.mover');
+ var items = document.getElementsByClassName('mover');
+  var latestKnownScrollY = window.scrollY;
+ 
+ for (var i = 0; i < items.length; i++) {
+    var phase = Math.sin((latestKnownScrollY / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
+
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
